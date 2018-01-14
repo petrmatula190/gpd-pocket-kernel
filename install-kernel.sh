bb6fc2f74638deaf359600de90c429ffbe4a0967
Peter <matlala.se@gmail.com>
2018-01-15 00:48:49 +0100

    Repair download from google drive
    wget cannot download file grom google drive, I replace it with curl.

--- install-kernel.sh
+++ install-kernel.sh
@@ -6,8 +6,10 @@ CURRENT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p /tmp/gpd-pocket-kernel
 cd /tmp/gpd-pocket-kernel
 
-echo "Downloading kernel files...."
-wget https://drive.google.com/file/d/1pUfjuMrWykNXGAey3YrT_xqlWJ0GYMD3/view?usp=drivesdk
+if [ ! -f "gpd-pocket-kernel-files.tar.gz" ]; then
+	 echo "Downloading kernel files...."
+	 curl -L https://drive.google.com/uc?id=1pUfjuMrWykNXGAey3YrT_xqlWJ0GYMD3 -o "gpd-pocket-kernel-files.tar.gz"
+fi
 
 echo "Extracting kernel files..."
 tar -xvzf  gpd-pocket-kernel-*.tar.gz 
@@ -18,7 +20,7 @@ sudo dpkg -i *.deb
 echo "Update grub..."
 sudo update-grub
 
-rm *.deb gpd-pocket-kernel-*.tar.gz 
+rm *.deb gpd-pocket-kernel-files.tar.gz 
 rm -rfd /tmp/gpd-pocket-kernel
 
 cd $CURRENT_DIR
